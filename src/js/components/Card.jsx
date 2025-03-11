da3aa1a402e994249019bd22d32a3ed2aa496a90
Moyunlimited <moy.estime@gmail.com>
2025-03-11 20:08:21 +0000

    finish

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Card = (props) => {
     return (
         <div className="text-center mx-2 w-25 mt-4 py-3">
-            <div className="card" style={{width: " 18rem;" }}>
+            <div className="card h-100" style={{width: " 18rem;" }}>
                 <img src={props.img} className="card-img-top" alt="#"/>
                     <div className="card-body">
                         <h5 className="card-title">{props.title}</h5>
