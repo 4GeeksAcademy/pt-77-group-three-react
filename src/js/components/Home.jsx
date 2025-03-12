0afb785f7902c08b0b8d95d493798aa4dffbcf1a
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2025-03-12 22:31:54 +0000

    jumbotron

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Jumbotron from "./Jumbotron";
 import Footer from "./Footer";
 
 const Home = () => {
 	return (
-		<div className="text-center">
+		<div className="mx-auto">
 			{/*  navbar */}
 			
 			{/*  jumbotron */}
-			
+			<div className="d-flex w-75 mx-auto bg-light">
+				<Jumbotron/>
+			</div>
 			{/*  cards */}
 			
 			{/* footer */}
